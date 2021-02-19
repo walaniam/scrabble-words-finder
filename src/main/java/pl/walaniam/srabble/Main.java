913422b75c8fc28a53b46f94ae61f38ccfcf087b
Mariusz Walania <mariusz.walania@gmail.com>
2021-02-19 10:51:25 +0100

    headless

--- src/main/java/pl/walaniam/srabble/Main.java
+++ src/main/java/pl/walaniam/srabble/Main.java
@@ -5,9 +5,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import java.util.Optional;
+
 @Slf4j
 @SpringBootApplication
 public class Main {
+
+    static {
+        String os = System.getProperty("os.name");
+        System.out.println("Running on: " + os);
+        if (!Optional.ofNullable(os).map(String::toLowerCase).orElse("").contains("windows")) {
+            System.setProperty("java.awt.headless", "true");
+        }
+    }
     
     public static void main(String[] args) {
         SpringApplicationBuilder builder = new SpringApplicationBuilder(Main.class);
